3f6ac50b6450fab1cbd871d9570f42adf1b70388
kenebaev <kutmankenebaev200@gmail.com>
2022-04-10 15:47:50 +0600

    first commit

---
+++ src/components/AddNote.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+
+const AddNote = ({handleAddNote}) => {
+
+    const [noteText, setNoteText] = useState('');
+    const characterLimit = 200;
+
+    const handleChange = (e) => {
+        if (characterLimit - e.target.value.length){
+            setNoteText(e.target.value);
+        }
+    };
+
+    const handleSaveClick = () => {
+        if(noteText.trim().length > 0) {
+            handleAddNote(noteText);
+            setNoteText('')
+        }
+    };
+
+    return(
+        <div className="note new">
+            <textarea
+                cols='10'
+                rows='8'
+                placeholder="Type to add a note..."
+                value={noteText}
+                onChange={handleChange}
+            > </textarea>
+            <div className="not-footer">
+                <small>{characterLimit - noteText.length} Remaining</small>
+                <button
+                    className="save"
+                    onClick={handleSaveClick}
+                >
+                    Save
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default AddNote
